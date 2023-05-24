79646e77aacb16814768690c7fa1ffd1a1861a33
joksina <jksalaudeen@yahoo.com>
2023-05-24 15:45:53 -0700

    updating keyboard

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -4,6 +4,18 @@ import STATUS from '../constants/status';
 
 const Qwerty = ({letters}) => {
 
+    const setClassName = (letter) => {
+        if(letter.status === STATUS.FOUND) {
+            return 'wordle-green';
+        } else if (letter.status === STATUS.CONTAINS) {
+            return 'wordle-yellow';
+        } else if (letter.status === STATUS.BAD) {
+            return 'wordle-gray';
+        } else {
+            return "";
+        }
+    }
+
     return (
         <div className="letter-wrapper">
             <div className="letter-buttons">
@@ -11,12 +23,12 @@ const Qwerty = ({letters}) => {
                     if(letter.letter !== undefined){
                     return(
                         <React.Fragment key={letter.id}>
-                            <button className={`keyboard-button`} 
+                            <button className={`keyboard-button ${setClassName(letter)}`} 
                                     key={letter.id}>
                                 {letter.letter}
                             </button>
-                            {letter.letter === "p" ? <pre></pre> : null}
-                            {letter.letter === "l" ? <pre></pre> : null}
+                            {letter.letter === "p" ? <br></br> : null}
+                            {letter.letter === "l" ? <br></br> : null}
                         </React.Fragment>
                     );
                     }
